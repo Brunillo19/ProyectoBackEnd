1f80e9fe9c66ff583a398c79595ce1c2af698019
Bruno Iglesias <iglesiasb11@gmail.com>
2023-02-04 18:33:57 -0300

    Entrega 2

--- Entrega2.js
+++ Entrega2.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 class ProductManager{
     constructor(path){
@@ -16,24 +17,44 @@ class ProductManager{
             return error
         }
     }
-    /* addProduct = ({title,description,price,thumbnail,code,stock})=>{
-        if (!title || (title=="") ||  !description ||(description=="") || !price ||(price=="") || !thumbnail ||(thumbnail=="")|| !code ||(code=="")|| !stock||(stock=="")) return console.log("Todos los campos son requeridos");
+    getProducts= async () => {
+        try {
+            return this.products = JSON.parse(await fs.promises.readFile(this.path,'utf-8'));
+        } catch (error) {
+            return error
+        }
+    }
 
-        const validateCode = this.products.some(product=>product.code === code);
-        if(validateCode) {
-            console.log( ("Este código ya existe"))}
-        else{
-        const id = this.products.length===0?1:this.products[this.products.length-1].id+1;
-        this.products.push({id,title,description,price,thumbnail,code,stock})}
-    } */
-    getProducts= ()=>{return this.products};
     getProductsbyId= async id =>
-    {   thisthis.products = JSON.parse(await fs.promises.readFile(this.path,'utf-8'));
+    {   this.products = JSON.parse(await fs.promises.readFile(this.path,'utf-8'));
         this.products.find(products=> products.id === id) || console.log("Not found");
     }
-    deleteProduct
+    deleteProduct =async id =>{
+        this.products = JSON.parse(await fs.promises.readFile(this.path,'utf-8'));
+        let ProdBor= this.products.find(product => product.id == id);
+        if (!ProdBor) {
+            return "El producto que desea borrar no existe"}
+        else {
+        this.products = this.products.filter(product => product.id !== id);
+        return "Producto borrado correctamente";}
+    }
+    updateProduct=async(id,data)=>{
+        this.products = JSON.parse(await fs.promises.readFile(this.path,'utf-8'));
+        let ProdMod = this.products.find(product => product.id == id);
+        console.log(ProdMod);
+        if (!ProdMod) return " Product Not Found";
+        let ProdIdx = this.products.findIndex(product => product.id == id);
+        this.products[ProdIdx] = { ...ProdMod, ...data };
+		await fs.promises.writeFile(this.path, JSON.stringify(this.products));
+		return 'Producto modificado correctamente';
+    };
+
 }
 const NuevoProducto = new ProductManager('./db/db.json');
+
+
+
+
 
 
 NuevoProducto.addProduct({
@@ -45,21 +66,12 @@ NuevoProducto.addProduct({
     stock:'4'
     
 })
-/* Añadiendo un producto con un campo faltante */
-NuevoProducto.addProduct({
-    title:'Monitor 22 Samsung',
-    description:'Este monitor de 22 pulgadas te va a resultar cómodo para estudiar trabajar o ver una película en tus tiempos de ocio.',
-    price:'14500',
-    thumbnail:'https://res.cloudinary.com/dfbuxinmw/image/upload/c_scale,w_400/v1670693774/00406004-177456-177456-01.png-177456-01_o7ew9c.png',
-    code:'134651',
-    
-    
-})
-/* Mostrando todos los productos añadidos */
 
 /* Buscando un id que no coincide */
 console.error(NuevoProducto.getProductsbyId(2)); 
 
 
-
+NuevoProducto.getProducts().then(res => console.log(res));
+NuevoProducto.deleteProduct(1).then(res=>console.log(res));
+NuevoProducto.updateProduct(2,{title:"Monitor 32 Bangho"})
 
