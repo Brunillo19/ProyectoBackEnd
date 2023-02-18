5b777955a7a59102d9bdc0e41035a9f2d8ce9906
Bruno Iglesias <iglesiasb11@gmail.com>
2023-02-18 17:03:11 -0300

    PreEntrega 1

---
+++ server_http/src/Routes/Cart.router.js
@@ -0,0 +1,23 @@
+
+const router = require('express').Router();
+const CartManager = require('../CartManager')
+const NewCart = new CartManager('db/carts.json')
+
+router.post('/',async(req,res)=> {
+	await NewCart.addCart();
+	res.status(200).json({message:'Carrito cargado correctamente'})
+})
+router.get('/:CartId', async (req, res) => {
+	const products = await NewCart.getProductsbyId(Number(req.params.CartId));
+	if (!products) return res.status(404).json({message:'No se encontró el carrito'});
+	res.status(200).json({products});
+});
+
+app.get('/:CartId/products/:ProductId', async (req, res) => {
+	await NewCart.addProductToCart(Numbre(req.params.CartId),Number(req.params.ProductId))
+	res.status(200).json({message:'Producto añadido satisfactoriamente'});
+});
+
+
+
+module.exports= router;
